33a31f7176dd0987438b42dedb9a661e1dd7ec8e
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2023-05-30 22:18:35 +0300

    Add story for Input component

---
+++ src/shared/ui/Input/ui/Input.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Input } from './Input';
+
+export default {
+  title: 'shared/Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  value: 'Input value',
+  placeholder: 'Username',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  value: 'Input value',
+  placeholder: 'Username',
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
